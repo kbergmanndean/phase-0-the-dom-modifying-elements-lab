705f738e8aee59f079e8ef7b57a06f95dd2257b4
Kaia <kbergmanndean@gmail.com>
2021-05-19 15:57:05 -0400

    commit tree 73f8836687d17434c67685c711b63129ba495243

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const main=document.querySelector('main');
+
+const main=document.querySelector("main")
 main.remove();
 
-const newHeader=document.createElement('h1');
+const newHeader=document.createElement("h1")
 document.body.appendChild(newHeader);
-
 newHeader.id="victory";
-
-newHeader.innerHTML="YOURNAME is the champion.";
+newHeader.innerHTML="YOURNAME is the champion";
